1deeacdf632056146c7cc2da6d512baac5a85ac3
Jean Cabral <jeancabral@outlook.com>
2017-11-25 23:13:57 -0300

    Criação das Cenas Outros Jogos, sobre Jogos e Principal

--- App.js
+++ App.js
@@ -1,50 +1,12 @@
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  Image,
-  View
 } from 'react-native';
-
-const logo = require('./src/imgs/logo.png');
-const btnJogar = require('./src/imgs/botao_jogar.png');
-const btnSobreJogo = require('./src/imgs/sobre_jogo.png');
-const btnOutrosJogos = require('./src/imgs/outros_jogos.png');
+import Principal from './src/components/Principal';
 
 export default class App extends Component<{}> {
   render() {
     return (
-      <View style={styles.cenaPrincipal}>
-
-        <View style={styles.apresentacaoJogo}>
-          <Image source={logo} />
-          <Image source={btnJogar} />
-        </View>
-
-        <View style={styles.rodape}>
-          <Image source={btnSobreJogo} />
-          <Image source={btnOutrosJogos} />
-        </View>
-
-      </View>
+      <Principal />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  cenaPrincipal: {
-    flex: 1,
-    backgroundColor: '#61BD8C',
-  },
-  apresentacaoJogo: {
-    flex: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  rodape: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-});
